ef43513ecae48934a329c33875143efb37ee01e3
chanutkid <chanut.kidw@gmail.com>
2024-09-08 22:14:21 +0700

    add : workflow2

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Header from './components/header'
+import Header from './components/Header'
 import Home from './components/Home'
 import About from './components/About'
 import Work from './components/Work'
